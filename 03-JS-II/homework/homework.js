291a824dbf3886888bdc901b215c8b08f20407b8
jhonca1 <jhonca1@live.com>
2022-03-12 09:46:39 -0500

    sabado

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -143,6 +143,8 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  
+
 }
 
 function esPrimo(numero) {
@@ -151,12 +153,30 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  if (numero < 2)
+    return false
+  if (numero === 2)
+    return true 
+  for (var i= 2 ; i < numero ; i++) {
+   if (numero % i === 0) {
+    return false;
+  }
 }
+    return true
+
+}
+ 
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+if (valor === true){
+  return "Soy verdadero"
+} else if (valor === false){
+  return "Soy falso"
+}
+
 
 }
 
@@ -164,19 +184,30 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  var tabladel6 = [];
+
+    for (var i = 0; i < 11 ; i++){
+              tabladel6.push (6 * i)
+        }
+     return  tabladel6
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero >= 100 && numero <= 999){
+    return true
+  }
+    return false
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+ 
+    
+
 }
 
 
